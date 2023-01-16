9ef79f98f8b36fe10730b85b2845b56068639056
foolish-pine <matsuda.takuya41@gmail.com>
2023-01-16 21:40:00 +0900

    chore: eslint-plugin-import、eslint-plugin-unused-imports追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: "next/core-web-vitals",
+  plugins: ["import", "unused-imports"],
+  rules: {
+    "import/order": [2, { alphabetize: { order: "asc" } }],
+    "unused-imports/no-unused-imports": "error",
+  },
 };
