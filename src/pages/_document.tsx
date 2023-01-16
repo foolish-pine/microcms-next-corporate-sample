4476c44b7360b1521cba882b4e139d0fc72d5a9e
foolish-pine <matsuda.takuya41@gmail.com>
2023-01-16 21:50:10 +0900

    feat: meta設定とfavicon追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="format-detection"
+          content="email=no,telephone=no,address=no"
+        />
+        <link rel="icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+        <link rel="icon" href="/img/android-chrome-192x192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
