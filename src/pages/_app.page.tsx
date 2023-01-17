4985049f4350e37091ac94aef22c1c69d034f9ac
foolish-pine <matsuda.takuya41@gmail.com>
2023-01-17 20:15:36 +0900

    feat: next-seo導入、DefaultSeo追加

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,13 +2,26 @@ import "destyle.css";
 import "@/styles/base.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { DefaultSeo } from "next-seo";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex" />
       </Head>
+      <DefaultSeo
+        defaultTitle="Shiro"
+        description="白いお花の専門店「Shiro」のホームページです。"
+        openGraph={{
+          type: "website",
+          title: "Shiro",
+          description: "白いお花の専門店「Shiro」のホームページです。",
+          site_name: "Shiro",
+          url: "https://microcms-next-corporate-sample.foolish-pine.com/",
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
